1e909b57625ba7b90ef8385e5b2d26afc71af93e
bugfire <bugfire@users.noreply.github.com>
2014-11-19 08:58:07 +0900

    updated graph

--- proxy.js
+++ proxy.js
@@ -2,36 +2,22 @@ var http = require('http');
 var url = require('url');
 http.createServer(function onCliReq(cliReq, cliRes) {
   var x = url.parse(cliReq.url);
-  var opt = {
-    host: 'AISEG_HOST',
-    port: 80,
-    method: cliReq.method,
-    headers: cliReq.headers
-  };
+  var ts = parseInt((new Date).getTime()/1000);
   var path;
-  var ts = parseInt((new Date)/1000);
-  switch (x.path) {
+  switch (x.path.substring(0, 2)) {
     case '/t':
-      path = "/get/top_val.cgi";
+      path = "/get/top_val.cgi?poll=" + ts;
       break;
-    case '/p0':
-      path = "/get/instantvaldata.cgi?pageno=0&poll=" + ts;
-      break;
-    case '/p1':
-      path = "/get/instantvaldata.cgi?pageno=1&poll=" + ts;
-      break;
-    case '/p2':
-      path = "/get/instantvaldata.cgi?pageno=2&poll=" + ts;
-      break;
-    case '/p3':
-      path = "/get/instantvaldata.cgi?pageno=3&poll=" + ts;
+    case '/p': // "/p0" .. "/p3"
+      index = x.path.substring(2, 3);
+      path = "/get/instantvaldata.cgi?pageno=" + index + "&poll=" + ts;
       break;
     case '/l':
       path = "/set/exectop2.cgi";
       break;
     default:
       cliRes.writeHead(404, {'Content-Type': 'text/html'});
-      cliRes.write("<html><body>404 Not Found</body></html>");
+      cliRes.write("<html><title>404 Not Found</title><body>404 Not Found</body></html>");
       cliRes.end();
       return;
   }
@@ -43,7 +29,7 @@ http.createServer(function onCliReq(cliReq, cliRes) {
       auth: 'AiSEG:AISEG_PASS'
     },
     function (res) {
-      res.headers['Access-Control-Allow-Origin'] = '*';
+      res.headers['Access-Control-Allow-Origin'] = '*'; // allow XSS
       cliRes.writeHead(res.statusCode, res.headers);
       res.pipe(cliRes);
     }
